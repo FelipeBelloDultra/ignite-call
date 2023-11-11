d02d6cc3c67267754b60e14fe5d1dd7196f885bd
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-11-11 10:36:35 -0300

    feat: :rocket: add react-hook-form

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -1,15 +1,31 @@
 import { Button, TextInput } from "@ignite-ui/react";
-import * as S from "./styles";
+import { useForm } from "react-hook-form";
 import { ArrowRight } from "phosphor-react";
+import { z } from "zod";
+
+import * as S from "./styles";
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+});
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>;
 
 export default function ClaimUserNameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data);
+  }
+
   return (
-    <S.Form as="form">
+    <S.Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size="sm"
         prefix="ignite.com/"
         placeholder="seu-usuario"
         crossOrigin=""
+        {...register("username")}
       />
       <Button size="sm" type="submit">
         Reservar
