e826a0397bb2db6810a5569c31a53bcb2e38dc15
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-11-15 19:53:34 -0300

    feat: :rocket: create time picker

--- src/pages/schedule/[username]/schedule-form/calendar-step/index.tsx
+++ src/pages/schedule/[username]/schedule-form/calendar-step/index.tsx
@@ -3,9 +3,36 @@ import { Calendar } from "../../../../../components/calendar";
 import * as S from "./styles";
 
 export function CalendarStep() {
+  const isDateSelected = true;
+
   return (
-    <S.Container>
+    <S.Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <S.TimePicker>
+          <S.TimePickerHeader>
+            terca-feira <span>20 de setembro</span>
+          </S.TimePickerHeader>
+
+          <S.TimePickerList>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+          </S.TimePickerList>
+        </S.TimePicker>
+      )}
     </S.Container>
   );
 }
